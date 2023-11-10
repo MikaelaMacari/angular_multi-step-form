2b0bc3fe2a03cbff045e0ee0d90b680569f34a2f
MikaelaMacari <55883910+MikaelaMacari@users.noreply.github.com>
2023-11-10 20:22:48 +0200

    Select plan (#4)
    * create card component, and select-plan component
    
    * create subscription-card component

---
+++ src/app/components/steps/select-plan/select-plan.component.ts
@@ -0,0 +1,39 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ButtonComponent} from "../../common/button/button.component";
+import {SectionHeaderComponent} from "../../common/section-header/section-header.component";
+import {CardComponent} from "../../common/card/card.component";
+import {SubscriptionCardComponent} from "../../common/subscription-card/subscription-card.component";
+
+@Component({
+  selector: 'app-select-plan',
+  standalone: true,
+  imports: [CommonModule, ButtonComponent, SectionHeaderComponent, CardComponent, SubscriptionCardComponent],
+  templateUrl: './select-plan.component.html',
+  styleUrls: ['./select-plan.component.scss']
+})
+export class SelectPlanComponent {
+  public plans: { imageUrl: string, title: string, price: number, plan: number, isActive: boolean }[] = [
+    {
+      imageUrl: 'assets/images/icon-arcade.svg',
+      title: 'Arcade',
+      price: 90,
+      plan: 2,
+      isActive: true,
+    },
+    {
+      imageUrl: 'assets/images/icon-advanced.svg',
+      title: 'Advanced',
+      price: 120,
+      plan: 2,
+      isActive: false,
+    },
+    {
+      imageUrl: 'assets/images/icon-pro.svg',
+      title: 'Pro',
+      price: 150,
+      plan: 2,
+      isActive: false,
+    },
+  ]
+}
